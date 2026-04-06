e76e2f1f50991ba0ebdc6c3f5529f378c0495606
CerinSeo <serin9470@gmail.com>
2026-04-06 21:40:10 +0900

    feat: initial iOS project setup

---
+++ sugarcat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sugarcat
+//
+//  Created by 서세린 on 4/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sugarcat/sugarcatApp.swift
@@ -0,0 +1,17 @@
+//
+//  sugarcatApp.swift
+//  sugarcat
+//
+//  Created by 서세린 on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct sugarcatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
