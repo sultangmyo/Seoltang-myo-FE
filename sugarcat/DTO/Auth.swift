8cbd0c7963dd44c42eeafebc92e31c2c4ab359a9
CerinSeo <serin9470@gmail.com>
2026-04-08 20:10:08 +0900

    1.1 apple 소셜로그인 커밋

--- sugarcat/DTO/Auth.swift
+++ sugarcat/DTO/Auth.swift
@@ -5,3 +5,17 @@
 //  Created by 서세린 on 4/8/26.
 //
 
+import Foundation
+
+// 1.1 apple 소셜로그인 request dto
+struct AppleLoginRequestDTO: Codable {
+    let identityToken: String
+}
+
+// 1.1 apple 소셜로그인 response dto
+struct AppleLoginResponseDTO: Codable {
+    let accessToken: String
+    let refreshToken: String
+    let isNewUser: Bool
+}
+
