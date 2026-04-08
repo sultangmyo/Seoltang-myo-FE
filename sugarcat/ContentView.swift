4c825d3baabfd02c170a4f5cd779e7ebc4e6408d
CerinSeo <serin9470@gmail.com>
2026-04-08 15:08:07 +0900

    feat: pr 시험용

--- sugarcat/ContentView.swift
+++ sugarcat/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("서브 타이틀")
+            .subTitle1B()
         }
         .padding()
     }
