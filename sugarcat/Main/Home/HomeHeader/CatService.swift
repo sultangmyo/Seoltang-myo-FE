d18567d868699215a119058eb8a4fbbb84f09d2d
CerinSeo <serin9470@gmail.com>
2026-04-15 21:08:16 +0900

    feat: userid 체크 로직 삭제

--- sugarcat/Main/Home/HomeHeader/CatService.swift
+++ sugarcat/Main/Home/HomeHeader/CatService.swift
@@ -9,12 +9,12 @@ import Foundation
 
 // 실제 cat serviceprotocol
 protocol CatServiceProtocol {
-    func fetchCatInfo(userId: String) async throws -> CatInfoResponseDTO
+    func fetchCatInfo() async throws -> CatInfoResponseDTO
 }
 
 // 해당 목업서비스 클래스는 백엔드 통신 이후 주석 처리 될 예정.
 final class MockCatService: CatServiceProtocol {
-    func fetchCatInfo(userId: String) async throws -> CatInfoResponseDTO {
+    func fetchCatInfo() async throws -> CatInfoResponseDTO {
         // 서버 대신 임시 데이터 반환
         return CatInfoResponseDTO(
             name: "나비",
--- sugarcat/Main/Home/HomeHeader/HomeHeaderViewModel.swift
+++ sugarcat/Main/Home/HomeHeader/HomeHeaderViewModel.swift
@@ -24,15 +24,9 @@ final class HomeHeaderViewModel: ObservableObject {
     // 데이터 로딩 함수
     func loadHeader() async {
         
-        // 1. userId 가져오기
-        guard let userId = UserSessionManager.shared.userId else {
-            titleText = "유저 정보 없음"
-            return
-        }
-        
         do {
             // 2. 고양이 정보 요청
-            let catInfo = try await catService.fetchCatInfo(userId: userId)
+            let catInfo = try await catService.fetchCatInfo()
             
             // 3. String → Date 변환
             guard let diagnosedDate = DateParser.parse(catInfo.diagnosedDate) else {
