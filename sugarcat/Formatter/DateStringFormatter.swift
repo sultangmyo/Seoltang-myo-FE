b4b07f8bea90b965e271f812e02e33f235b106cc
CerinSeo <serin9470@gmail.com>
2026-05-01 16:56:35 +0900

    입력뷰 날짜 표시 포매터

--- sugarcat/Formatter/DateStringFormatter.swift
+++ sugarcat/Formatter/DateStringFormatter.swift
@@ -38,4 +38,14 @@ enum DateStringFormatter {
         
         return formatter.string(from: date)
     }
+    
+    // MARK: - Date -> yyyy. M. d. (E) 혈당입력뷰, 식사 입력뷰에 사용
+    static func displayKoreanDate(from date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy. M. d. (E)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        
+        return formatter.string(from: date)
+    }
 }
