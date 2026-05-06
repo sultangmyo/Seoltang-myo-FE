e16725be0d0d3b310bfaab9a476a7bee516fcfe4
CerinSeo <serin9470@gmail.com>
2026-05-06 15:29:29 +0900

    feat: 마이페이지 구현

--- sugarcat/Main/MyPage/MyPageView.swift
+++ sugarcat/Main/MyPage/MyPageView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct MyPageView: View {
-    var body: some View {
-        Text("마이페이지")
-    }
+    //네비게이션
+    @State private var path = NavigationPath()
     
+    var body: some View {
+        NavigationStack(path: $path) {
+            //네비게이션 헤더
+            VStack (spacing: 0){
+                VStack(spacing: 0) {
+                    NavigationHeaderView(title: "마이페이지")
+                    Divider()
+                }
+                ScrollView {
+                    // MARK: - divider 상단 영역
+                    //여기서 구현 하시면 됩니다
+                    VStack (spacing: 0){
+                        
+                    }
+                    
+                    //커스텀 divider 적용
+                    CustomDividerView()
+                    
+                    //divider 하단 영역 뷰 구현
+                    DividerBottomSection()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+
+        MyPageView()
+
+    }
 }
