b992a2d220be3a0934e57c29c4c60ea1cf9476fb
jo su hyun <cocoball329@gmail.com>
2026-05-09 18:45:24 +0900

    Feat : 로그인 화면 분기 설정

--- sugarcat/OnBording/OnBordingContainerView.swift
+++ sugarcat/OnBording/OnBordingContainerView.swift
@@ -1,42 +1,50 @@
 //
-//  OnBordingContainerView.swift
+//  OnBordingContainer.swift
 //  sugarcat
 //
-//  Created by 野菜サンド on 4/14/26.
+//  Created by 野菜サンド on 5/7/26.
 //
+
 import SwiftUI
 
-// 온보딩 페이지 열거형
+
 enum OnboardingPage: Hashable {
-    //닉네임 입력 뷰
+    case login
     case nickname
-    //고양이 분기 선택 뷰
     case catSetup
-    // 고양이 기본 정보 입력 뷰
     case catProfile
-    // 고양이 초대 입력 뷰
     case catInvite
 }
 
 struct OnBoardingContainerView: View {
     @State private var path = NavigationPath()
+    
+   
+    var nextAction: () -> Void
+    var finishAction: () -> Void
 
     var body: some View {
         NavigationStack(path: $path) {
-            NicknameInputView(path: $path)
-                .navigationDestination(for: OnboardingPage.self) { page in
-                    switch page {
-                    case .nickname:
-                        NicknameInputView(path: $path)
-                    case .catSetup:
-                        CatSetupView(path: $path)
-                    case .catProfile:
-                        CatInfoInputView(path: $path)
-                    case .catInvite:
-                        CatInviteView()
-                    }
-                    }
+           
+            LoginView(nextAction: {
+                path.append(OnboardingPage.nickname)
+            }, finishAction: {
+                finishAction()
+            })
+            .navigationDestination(for: OnboardingPage.self) { page in
+                switch page {
+                case .login:
+                    LoginView(nextAction: { path.append(OnboardingPage.nickname) }, finishAction: finishAction)
+                case .nickname:
+                    NicknameInputView(path: $path)
+                case .catSetup:
+                    CatSetupView(path: $path)
+                case .catProfile:
+                    CatInfoInputView(path: $path)
+                case .catInvite:
+                    CatInviteView()
                 }
+            }
         }
     }
-
+}
--- sugarcat/sugarcatApp.swift
+++ sugarcat/sugarcatApp.swift
@@ -6,13 +6,30 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct sugarcatApp: App {
+    
+    //앱이 실행될때 카카오 SDK 세팅
+    init() {
+        KakaoSDK.initSDK(appKey: "36c5625f91b1e18acd05b5d9abe41cbc")
+    }
         
     var body: some Scene {
         WindowGroup {
-            OnBoardingContainerView()
+            OnBoardingContainerView(nextAction: {
+                            print("로그인 성공 -> 온보딩")
+                        }, finishAction: {
+                            print("이미 가입된 유저 -> 메인 화면")
+                        })
+                .onOpenURL { url in
+                    if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
         }
     }
 }
+
