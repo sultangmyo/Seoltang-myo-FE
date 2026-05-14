f44aaf41e246fc117953e009aa16604d146c1f47
jo su hyun <cocoball329@gmail.com>
2026-05-14 18:45:07 +0900

    feat : 인증 인가 엔드포인트 작성

--- sugarcat/Network/EndPoint/AuthEP.swift
+++ sugarcat/Network/EndPoint/AuthEP.swift
@@ -1,39 +1,60 @@
-//
-//  AuthEP.swift
-//  sugarcat
-//
-//  Created by 서세린 on 5/13/26.
-//
 
-//import Foundation
-//
-//enum Endpoint {
-//    // MARK: - User
-//    case fetchCurrentUserInfo
-//    case updateNickname
-//}
-//
-//extension Endpoint {
-//    
-//    var path: String {
-//        switch self {
-//        //7.1 사용자이름, 고양이 닉네임 조회
-//        case .fetchCurrentUserInfo:
-//            return "/api/v1/users/me/"
-//        
-//        //7.2 사용자 닉네임 수정
-//        case .updateNickname:
-//            return "/api/v1/users/me/nickname"
-//        }
-//    }
-//    
-//    var method: HTTPMethod {
-//        switch self {
-//        case .fetchCurrentUserInfo:
-//            return .get
-//            
-//        case .updateNickname:
-//            return .patch
-//        }
-//    }
-//}
+ // AuthEP.swift
+ // sugarcat
+
+ // Created by 서세린 on 5/13/26.
+
+
+import Foundation
+
+enum AuthEndpoint {
+    // MARK: - AUTH
+    case onBoardingCompleted
+    //온보딩 완료 여부 검사
+    case onBoardingCheck
+    case apnsDeviceToken
+    case jwtRefresh
+    case logout
+    
+}
+
+extension AuthEndpoint {
+    
+    var path: String {
+        switch self {
+        // 1.4 온보딩 완료 여부 저장, 온보딩 완료 여부 검사
+        case .onBoardingCompleted, .onBoardingCheck:
+            return "/api/v1/auth/onboarding"
+        
+        // 1.0 apns device Token 여부 검사
+        case .apnsDeviceToken:
+            return "/api/v1/notifications/device-token"
+           
+        // 1.3 jwt 재발급
+        case .jwtRefresh:
+            return "/api/v1/auth/refresh"
+        // 1.1.2 로그아웃
+        case .logout:
+            return "/api/v1/auth/logout"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .onBoardingCompleted:
+            return .post
+            
+        case .onBoardingCheck:
+            return .get
+            
+        case .apnsDeviceToken:
+            return .post
+            
+        case .jwtRefresh:
+            return .post
+            
+        case .logout:
+            return .post
+        }
+    }
+}
