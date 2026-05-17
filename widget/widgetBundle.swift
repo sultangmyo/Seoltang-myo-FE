a3beccf25a4cd09c5a74fb725978f856b772c789
CerinSeo <serin9470@gmail.com>
2026-05-18 00:07:20 +0900

    feat: configure widget bundle entry point

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by 서세린 on 5/17/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+    }
+}
