bdb5292bedbda7da06c151e4921ef486c282be3c
CerinSeo <serin9470@gmail.com>
2026-05-26 21:55:26 +0900

    feat: 그래프 홈화면에 주입

--- sugarcat/Main/Home/HomeView.swift
+++ sugarcat/Main/Home/HomeView.swift
@@ -19,11 +19,20 @@ struct HomeView: View {
             // 네비게이션
             NavigationHeaderView(title: viewModel.titleText)
             Divider()
-            Spacer()
+            
             //그래프
+            GraphTabView(
+                bloodSugarService: MockBloodSugarService()
+            )
+            
+            //커스텀 divider 적용
+            CustomDividerView()
+                .padding(.top, 35)
+                .padding(.bottom, 16)
             
             //인슐린 투여기록
             InsulinChecklistSectionView(insulinService: MockInsulinService())
+            Spacer()
 
         }
         .background(Color.white)
