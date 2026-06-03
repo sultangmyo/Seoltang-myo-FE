bf1f3ff19a8e3f7cb82f2df0659959907af33000
CerinSeo <serin9470@gmail.com>
2026-06-03 15:06:26 +0900

    feat: homeview scrollview로 변환

--- sugarcat/Main/Home/HomeView.swift
+++ sugarcat/Main/Home/HomeView.swift
@@ -16,24 +16,26 @@ struct HomeView: View {
     
     var body: some View {
         VStack(spacing: 0) {
-            // 네비게이션
-            NavigationHeaderView(title: viewModel.titleText)
-            Divider()
-            
-            //그래프
-            GraphTabView(
-                bloodSugarService: MockBloodSugarService()
-            )
-            
-            //커스텀 divider 적용
-            CustomDividerView()
-                .padding(.top, 35)
-                .padding(.bottom, 16)
-            
-            //인슐린 투여기록
-            InsulinChecklistSectionView(insulinService: MockInsulinService())
-            Spacer()
-
+            VStack(spacing:0) {
+                // 네비게이션
+                NavigationHeaderView(title: viewModel.titleText)
+                Divider()
+            }
+            ScrollView{
+                //그래프
+                GraphTabView(
+                    bloodSugarService: MockBloodSugarService()
+                )
+                
+                //커스텀 divider 적용
+                CustomDividerView()
+                    .padding(.top, 35)
+                    .padding(.bottom, 16)
+                
+                //인슐린 투여기록
+                InsulinChecklistSectionView(insulinService: RealInsulinService())
+                Spacer()
+            }
         }
         .background(Color.white)
         .task {
