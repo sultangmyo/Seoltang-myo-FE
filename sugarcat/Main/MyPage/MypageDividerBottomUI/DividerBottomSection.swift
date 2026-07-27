44368bae8bc1f46e075ddfca7d269e73d3ba8041
CerinSeo <serin9470@gmail.com>
2026-07-27 22:34:01 +0900

    feat: 텍스트에 버전증가

--- sugarcat/Main/MyPage/MypageDividerBottomUI/DividerBottomSection.swift
+++ sugarcat/Main/MyPage/MypageDividerBottomUI/DividerBottomSection.swift
@@ -100,7 +100,7 @@ struct DividerBottomSection: View {
                     )
                     Spacer()
                     // 버전 적어 놨습니다.
-                    Text("v.1.0.0")
+                    Text("v.1.0.1")
                         .caption2R()
                         .foregroundColor(.gray1)
                 }
